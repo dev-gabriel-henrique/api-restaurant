78be690a6cd7de28aef381602065b778a6164382
Gabriel Henrique <work.gabriel12@gmail.com>
2024-10-30 09:55:16 -0300

    Projeto finalizado - Adicionado novas rotas de POST, PUT, PATCH, GET e DELETE para as páginas de orders, tables e tables-sessions; - Adicionado seed para inserir dados na tabela de tables e products; - Adicionado migrations das tabelas de orders, tables e tables-sessions; - Adicionado tipagens para as tabelas de tables, orders e tables-sessions; - Adicionado requests do insomnia;

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,7 +1,14 @@
 import { Router } from "express";
+
 import { productsRoutes } from "./products-routes";
+import { tablesRoutes } from "./tables-routers";
+import { tablesSessionsRoutes } from "./tables-sessions-routes";
+import { ordersRoutes } from "./orders-routes";
 
 const routes = Router()
+routes.use("/tables", tablesRoutes)
+routes.use("/orders", ordersRoutes)
 routes.use("/products", productsRoutes)
+routes.use("/tables-sessions", tablesSessionsRoutes)
 
 export { routes }
